4ebc714f0486a6cba0287e1082c58947f94b98e8
Priyanka Biswas <priyankabiswasonly1@gmail.com>
2021-06-17 16:11:01 +0530

    first_commit

---
+++ enroll/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import SignUpForm, EditUserProfileForm, EditAdminProfileForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.models import User, Group
+
+# Create your views here.
+
+# signUp
+
+def home(request):
+    return render(request, 'enroll/home.html')
+
+def sign_up(request):
+    if request.method == 'POST':
+        fm = SignUpForm(request.POST)
+        if fm.is_valid():
+            messages.success(request,'Acount Created Successfully!!!')
+            user = fm.save()
+            group = Group.objects.get(name = 'Editor')
+            user.groups.add(group)
+    
+    else:
+        fm = SignUpForm()
+
+    return render(request, 'enroll/signup.html', {'form':fm})
+ 
+
+# login
+
+def user_login(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            fm = AuthenticationForm(request=request, data=request.POST)
+            if fm.is_valid():
+                uname = fm.cleaned_data['username']
+                upass = fm.cleaned_data['password']
+                user = authenticate(username=uname, password=upass)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, 'Logged in successfully!!!')
+                    return HttpResponseRedirect('/dashboard/')
+
+        else:
+            fm = AuthenticationForm()
+
+        return render(request, 'enroll/userlogin.html',{'form':fm})
+    else:
+        return HttpResponseRedirect('/dashboard/')
+
+# profile
+
+def user_dashboard(request):
+    if request.user.is_authenticated:
+        return render(request, 'enroll/dashboard.html',{'name':request.user.username})
+
+    else:
+        return HttpResponseRedirect('/login/')
+# logout
+
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/login/')
+
